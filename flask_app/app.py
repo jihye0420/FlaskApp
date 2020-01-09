a87ea3c55f647cd96fef3d3221c63a30731793c4
jihye0420 <gh06095@naver.com>
2020-01-09 13:17:37 +0900

    flask commit

---
+++ flask_app/app.py
@@ -0,0 +1,38 @@
+from flask import Flask, escape, request
+
+app = Flask(__name__)
+
+# @app.route('/')
+# def hello():
+#     name = request.args.get("name", "World")
+#     return f'Hello, {escape(name)}!'
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % escape(username)
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return 'Subpath %s' % escape(subpath)
+
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if request.method == 'POST':
+#         return do_the_login()
+#     else:
+#         return show_the_login_form()
